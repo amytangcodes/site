fa6ff978c66286f2934fd27c629863d6b6dfe897
Amy Tang <amytang77@gmail.com>
2018-01-01 13:45:05 -0500

    Init commit

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,103 @@
+/*!
+ * fastshell
+ * Fiercely quick and opinionated front-ends
+ * https://HosseinKarami.github.io/fastshell
+ * @author Hossein Karami
+ * @version 1.0.5
+ * Copyright 2018. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // FastShell
+  });
+
+  // $(function () {
+  //   $(window.scroll(function() {
+  //     var scroll = $(window).scrollTop();
+  //     $('#header').css('transform','translateY(' +  (scroll/2)  + 'px)');
+  //   }));
+  // });
+
+  function scrollFooter(scrollY, heightFooter) {
+    console.log(scrollY);
+    console.log(heightFooter);
+
+    if(scrollY >= heightFooter) {
+      $('footer').css({
+        'bottom' : '0px'
+      });
+    }
+    else
+    {
+      $('footer').css({
+        'bottom' : '-' + heightFooter + 'px'
+      });
+    }
+  }
+
+  $(window).load(function(){
+    var windowHeight      = $(window).height(),
+      footerHeight        = $('footer').height(),
+      heightDocument      = (windowHeight) + ($('.main').height()) - 10;
+
+    // Definindo o tamanho do elemento pra animar
+    $('#scroll-animate, #scroll-animate-main').css({
+      'height' :  heightDocument + 'px'
+    });
+
+    // Definindo o tamanho dos elementos header e conteúdo
+    // $('header').css({
+    //   'height' : windowHeight + 'px',
+    //   'line-height' : windowHeight + 'px'
+    // });
+
+    $('.wrapper-parallax').css({
+      'margin-top' : windowHeight + 'px'
+    });
+
+    scrollFooter(window.scrollY, footerHeight);
+
+    // ao dar rolagem
+    window.onscroll = function(){
+      var scroll = window.scrollY;
+
+      $('#scroll-animate-main').css({
+        'top' : '-' + scroll + 'px'
+      });
+
+      // $('header').css({
+      //   'background-position-y' : 50 - (scroll * 100 / heightDocument) + '%'
+      // });
+
+      scrollFooter(scroll, footerHeight);
+    };
+  });
+
+  $(document).ready(function() {
+    $('.skillbar').each(function() {
+      $(this)
+        .find('.skillbar__bar')
+        .animate({
+            width: $(this).attr('data-percent')
+          },
+          6000
+        );
+    });
+  });
+
+
+
+  $(document).bind('scroll', function(e) {
+    var scrollOffset = $(document).scrollTop();
+    var containerOffset = $('.skillbar').offset().top - window.innerHeight;
+    if (scrollOffest > containerOffset) {
+      var animate = setInterval(function() {
+        loading();
+      }, 6000);
+    }
+  });
+
+})(jQuery, window, document);
